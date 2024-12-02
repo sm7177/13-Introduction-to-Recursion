f60582ec73e5c5db20a7b768e19033cdc6251b31
sm7177 <subrinamaharjan2020@gmail.com>
2024-12-02 18:51:59 +0545

    .

--- main.py
+++ main.py
@@ -38,11 +38,36 @@
 
 
 
-def fact(n):
-    if(n==0 or n==1):
-        return 1
+# def fact(n):
+#     if(n==0 or n==1):
+#         return 1
     
-    return n*fact(n-1)
+#     return n*fact(n-1)
 
-n=int(input("Enter a number:"))
-print(fact(n))
+# n=int(input("Enter a number:"))
+# print(fact(n))
+
+
+
+
+
+
+
+
+
+
+
+def recursive_fibonacci(n):
+    if n<=1:
+        return n
+    else:
+        return(recursive_fibonacci(n-1) + recursive_fibonacci(n-2))
+    
+n_terms=10
+
+if n_terms<=0:
+    print("Invalid input! Please enter a positive number.")
+else:
+    print("Fibonacci Series:")
+for i in range(n_terms):
+    print(recursive_fibonacci(i))
